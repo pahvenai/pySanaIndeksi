d8cae2dd7329e5aaaf8988af090c902c6f9344a1
Patrik Ahvenainen <patrik.ahvenainen@gmail.com>
2012-08-02 12:13:16 +0300

    Added first unit test

--- src/pyUnitTest.py
+++ src/pyUnitTest.py
@@ -1,11 +1,17 @@
-# To change this template, choose Tools | Templates
-# and open the template in the editor.
+# -*- coding: utf-8 -*-
 
 import unittest
+from WordReader import WordReader, sanitize
+from RedBlack import RedBlack
+from Trie import Trie
+
+unsanitizedWords = ['32verregö ', 'dseFRfw ert', '  dfg', '¤gf', '4egdFB']
+sanitizedWords =   ['32VERREG'  , 'DSEFRFW'    , 'DFG'  , ''   , '4EGDFB']
 
 
 class  PyUnitTestCase(unittest.TestCase):
     #def setUp(self):
+    #    self.lukija = WordReader("../Material/Grimm's Fairy Tales.txt")
     #    self.foo = PyUnit()
     #
 
@@ -13,10 +19,16 @@ class  PyUnitTestCase(unittest.TestCase):
     #    self.foo.dispose()
     #    self.foo = None
 
-    def test_pyUnit(self):
-        #assert x != y;
-        #self.assertEqual(x, y, "Msg");
-        self.fail("TODO: Write test")
+    
+
+    def testSanitize(self):
+        """ Test whether word sanitizing works """
+        testWords = []
+        for word in unsanitizedWords:
+            testWords.append(sanitize(word))
+        print testWords
+        print sanitizedWords
+        self.assertEqual(testWords, sanitizedWords, 'Failed to sanitize words')
 
 if __name__ == '__main__':
     unittest.main()
